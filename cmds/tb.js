edccb5ac78df2b790485f77195930ca171ba122f
Luke Sullivan <lukes@flow-motion.io>
2026-05-19 17:00:21 +0100

    add code for other instances on tb

--- cmds/tb.js
+++ cmds/tb.js
@@ -8,3 +8,12 @@
 
   window.open(`/now/nav/ui/classic/params/target/${table}_list.do`, '_blank');
 })();
+
+/*
+Some older instances
+(function () {
+  const table = '$0';
+
+  window.open(`/nav_to.do?uri=%2F${table}_list.do`, '_blank');
+})();
+*/
